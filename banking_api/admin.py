62fc6376cca96b1a130debf78761211571d5a241
loke <lokendar384@gmail.com>
2025-06-19 21:49:20 +0545

    some adminsitechange

--- banking_api/admin.py
+++ banking_api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Account, Transaction, Loan
 
-# Register your models here.
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ['account_number', 'user', 'account_type', 'balance', 'is_active']
+    search_fields = ['account_number', 'user__username']
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['transaction_type', 'account', 'amount', 'timestamp']
+    search_fields = ['account__account_number']
+
+@admin.register(Loan)
+class LoanAdmin(admin.ModelAdmin):
+    list_display = ['user', 'amount', 'status', 'applied_at']
+    search_fields = ['user__username']
